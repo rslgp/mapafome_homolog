82d7b2af97bff1afddfb7c6c5b7e41b2a12aa8c8
rslgp <rslgp@cin.ufpe.br>
2026-07-03 18:17:48 -0300

    Updates

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * the user sees UI feedback. This SW is read-biased.
  */
 
-const SW_VERSION = '1.0.0-1783032126128-7f1a7dd';
+const SW_VERSION = '1.0.0-1783113415555-3f8bcec';
 const SHELL_CACHE = `${SW_VERSION}-shell`;
 const TILE_CACHE  = `${SW_VERSION}-tiles`;
 const DATA_CACHE  = `${SW_VERSION}-data`;
